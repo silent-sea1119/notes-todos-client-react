a841cdb796cb66e2031b93887fa84f65f703e1e8
silent-sea1119 <sandragonzales56t@gmail.com>
2022-05-26 10:50:38 +0100

    Updated

--- src/components/activity-comps/NoteCard/NoteCard.tsx
+++ src/components/activity-comps/NoteCard/NoteCard.tsx
@@ -5,9 +5,9 @@ const NotesCard = () => {
   return (
     <div className="note-card-wrapper">
       <div className="note-card">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-        sapiente vitae non exercitationem delectus suscipit quis vel illum
-        placeat itaque?
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab corrupti
+        eum ipsam odit iusto quaerat ex, totam ducimus, laborum distinctio amet
+        quia iure alias perferendis.
       </div>
     </div>
   );
