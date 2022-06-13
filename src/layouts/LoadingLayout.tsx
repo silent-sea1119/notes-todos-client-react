c03e8e35805324046f02f285ad4d4e8b12cbc28c
silent-sea1119 <sandragonzales56t@gmail.com>
2022-06-13 09:53:24 +0100

    Added header theme color and preloader

--- src/layouts/LoadingLayout.tsx
+++ src/layouts/LoadingLayout.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import "./LoadingLayout.scss";
 
 export default function LoadingLayout() {
-  return <div>Loading Data....</div>;
+  return (
+    <div className="loading-layout">
+      <div className="building-blocks-cover">
+        <div className="building-blocks">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 }
